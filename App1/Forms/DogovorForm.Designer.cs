97e5b83cbd0df1fbbc71e3a2210af4769fdaef4e
Ruslan <ruzyqwerty@gmail.com>
2021-06-05 23:23:27 +0300

    оформил окно оформление договора

--- App1/Forms/DogovorForm.Designer.cs
+++ App1/Forms/DogovorForm.Designer.cs
@@ -33,21 +33,45 @@ namespace App1.Forms
             this.kursNameText = new System.Windows.Forms.Label();
             this.acceptBtn = new System.Windows.Forms.Button();
             this.cancelBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chasiText = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.priceText = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.phoneText = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.adresText = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.orgNameText = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.rukovodText = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.prepodPhoneText = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.stazText = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.prepodNameText = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // kursNameLabel
             // 
             this.kursNameLabel.AutoSize = true;
-            this.kursNameLabel.Location = new System.Drawing.Point(13, 13);
+            this.kursNameLabel.Location = new System.Drawing.Point(6, 18);
             this.kursNameLabel.Name = "kursNameLabel";
-            this.kursNameLabel.Size = new System.Drawing.Size(113, 17);
+            this.kursNameLabel.Size = new System.Drawing.Size(72, 17);
             this.kursNameLabel.TabIndex = 0;
-            this.kursNameLabel.Text = "Название курса";
+            this.kursNameLabel.Text = "Название";
             // 
             // kursNameText
             // 
             this.kursNameText.AutoSize = true;
-            this.kursNameText.Location = new System.Drawing.Point(16, 34);
+            this.kursNameText.Location = new System.Drawing.Point(9, 35);
             this.kursNameText.Name = "kursNameText";
             this.kursNameText.Size = new System.Drawing.Size(69, 17);
             this.kursNameText.TabIndex = 1;
@@ -55,7 +79,7 @@ namespace App1.Forms
             // 
             // acceptBtn
             // 
-            this.acceptBtn.Location = new System.Drawing.Point(51, 392);
+            this.acceptBtn.Location = new System.Drawing.Point(38, 470);
             this.acceptBtn.Name = "acceptBtn";
             this.acceptBtn.Size = new System.Drawing.Size(75, 23);
             this.acceptBtn.TabIndex = 2;
@@ -64,7 +88,7 @@ namespace App1.Forms
             // 
             // cancelBtn
             // 
-            this.cancelBtn.Location = new System.Drawing.Point(231, 392);
+            this.cancelBtn.Location = new System.Drawing.Point(223, 470);
             this.cancelBtn.Name = "cancelBtn";
             this.cancelBtn.Size = new System.Drawing.Size(75, 23);
             this.cancelBtn.TabIndex = 3;
@@ -72,20 +96,235 @@ namespace App1.Forms
             this.cancelBtn.UseVisualStyleBackColor = true;
             this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.priceText);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.chasiText);
+            this.groupBox1.Controls.Add(this.kursNameLabel);
+            this.groupBox1.Controls.Add(this.kursNameText);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 133);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Всё о курсе";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кол-во часов";
+            // 
+            // chasiText
+            // 
+            this.chasiText.AutoSize = true;
+            this.chasiText.Location = new System.Drawing.Point(9, 69);
+            this.chasiText.Name = "chasiText";
+            this.chasiText.Size = new System.Drawing.Size(69, 17);
+            this.chasiText.TabIndex = 3;
+            this.chasiText.Text = "testestest";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Цена";
+            // 
+            // priceText
+            // 
+            this.priceText.AutoSize = true;
+            this.priceText.Location = new System.Drawing.Point(12, 103);
+            this.priceText.Name = "priceText";
+            this.priceText.Size = new System.Drawing.Size(69, 17);
+            this.priceText.TabIndex = 5;
+            this.priceText.Text = "testestest";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.rukovodText);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.phoneText);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.adresText);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.orgNameText);
+            this.groupBox2.Location = new System.Drawing.Point(12, 151);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(311, 174);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Всё об организации";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Номер телефона";
+            // 
+            // phoneText
+            // 
+            this.phoneText.AutoSize = true;
+            this.phoneText.Location = new System.Drawing.Point(12, 103);
+            this.phoneText.Name = "phoneText";
+            this.phoneText.Size = new System.Drawing.Size(69, 17);
+            this.phoneText.TabIndex = 5;
+            this.phoneText.Text = "testestest";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Адрес";
+            // 
+            // adresText
+            // 
+            this.adresText.AutoSize = true;
+            this.adresText.Location = new System.Drawing.Point(9, 69);
+            this.adresText.Name = "adresText";
+            this.adresText.Size = new System.Drawing.Size(69, 17);
+            this.adresText.TabIndex = 3;
+            this.adresText.Text = "testestest";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Название";
+            // 
+            // orgNameText
+            // 
+            this.orgNameText.AutoSize = true;
+            this.orgNameText.Location = new System.Drawing.Point(9, 35);
+            this.orgNameText.Name = "orgNameText";
+            this.orgNameText.Size = new System.Drawing.Size(69, 17);
+            this.orgNameText.TabIndex = 1;
+            this.orgNameText.Text = "testestest";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 17);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Руководитель";
+            // 
+            // rukovodText
+            // 
+            this.rukovodText.AutoSize = true;
+            this.rukovodText.Location = new System.Drawing.Point(15, 137);
+            this.rukovodText.Name = "rukovodText";
+            this.rukovodText.Size = new System.Drawing.Size(69, 17);
+            this.rukovodText.TabIndex = 7;
+            this.rukovodText.Text = "testestest";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.prepodPhoneText);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.stazText);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.prepodNameText);
+            this.groupBox3.Location = new System.Drawing.Point(12, 331);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(311, 133);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Всё о преподавателе";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Номер телефона";
+            // 
+            // prepodPhoneText
+            // 
+            this.prepodPhoneText.AutoSize = true;
+            this.prepodPhoneText.Location = new System.Drawing.Point(12, 103);
+            this.prepodPhoneText.Name = "prepodPhoneText";
+            this.prepodPhoneText.Size = new System.Drawing.Size(69, 17);
+            this.prepodPhoneText.TabIndex = 5;
+            this.prepodPhoneText.Text = "testestest";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 17);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Стаж";
+            // 
+            // stazText
+            // 
+            this.stazText.AutoSize = true;
+            this.stazText.Location = new System.Drawing.Point(9, 69);
+            this.stazText.Name = "stazText";
+            this.stazText.Size = new System.Drawing.Size(69, 17);
+            this.stazText.TabIndex = 3;
+            this.stazText.Text = "testestest";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 18);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(72, 17);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Название";
+            // 
+            // prepodNameText
+            // 
+            this.prepodNameText.AutoSize = true;
+            this.prepodNameText.Location = new System.Drawing.Point(9, 35);
+            this.prepodNameText.Name = "prepodNameText";
+            this.prepodNameText.Size = new System.Drawing.Size(69, 17);
+            this.prepodNameText.TabIndex = 1;
+            this.prepodNameText.Text = "testestest";
+            // 
             // DogovorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 450);
+            this.ClientSize = new System.Drawing.Size(346, 505);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cancelBtn);
             this.Controls.Add(this.acceptBtn);
-            this.Controls.Add(this.kursNameText);
-            this.Controls.Add(this.kursNameLabel);
             this.Name = "DogovorForm";
             this.Text = "DogovorForm";
             this.Load += new System.EventHandler(this.DogovorForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -95,5 +334,26 @@ namespace App1.Forms
         private System.Windows.Forms.Label kursNameText;
         private System.Windows.Forms.Button acceptBtn;
         private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label priceText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label chasiText;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label rukovodText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label phoneText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label adresText;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label orgNameText;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label prepodPhoneText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label stazText;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label prepodNameText;
     }
 }
